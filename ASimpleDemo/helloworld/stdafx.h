9052aaed272045e2e71ec842c3d33bdb87b4be29
sheeniya <jinjunjiex@foxmail.com>
2020-02-01 11:42:00 +0800

    helloworld

---
+++ ASimpleDemo/helloworld/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define  _CRT_SECURE_NO_WARNINGS
+#define     DLL_SOUI   //SOUI是以DLL提供时需要定义这个宏
+
+#include <souistd.h>
+#include <core/SHostDialog.h>
+#include <control/SMessageBox.h>
+#include <control/souictrls.h>
+
+using namespace SOUI;
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # a-simple-demo
 
-1.test
+这是一个用soui做的简单demo,用于个人练习和娱乐
+
+SOUI教程：https://home.cnblogs.com/u/setoutsoft/
+SOUI论坛：http://www.soui.vip/
+在编译SOUI时一定要看论坛里的视频教程
+
+1.2020/2/1 11：30
+目前代码全部来源于https://www.cnblogs.com/setoutsoft/p/3928361.html，后续将以此为基础做自己的demo
